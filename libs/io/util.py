4003fd85d13d2ec67b7aa2dbf2957a8646fa0155
zy <zhengyunuist@gmail.com>
2019-09-23 13:51:42 +0800

    some useful tools add in util

--- libs/io/util.py
+++ libs/io/util.py
@@ -1,7 +1,11 @@
+import sys
+sys.path.append("../../")
 import struct
 import bz2
 import gzip
 import datetime
+import os
+from configure.location_config import radar_info
 
 def _structure_size(structure):
     """计算structure的字节大小"""
@@ -60,3 +64,69 @@ def julian2date(JulianDate, Msec):
     deltsec = datetime.timedelta(milliseconds=Msec)
     scantime = datetime.datetime(1969, 12, 31) + deltday + deltsec
     return scantime
+
+def julian2date_SEC(Sec, Msec):
+    """
+    faster than num2date in netcdf4
+    :param Sec: seconds
+    :param Msec: microseconds
+    :return:
+    """
+    deltSec = datetime.timedelta(seconds=Sec)
+    deltMSec = datetime.timedelta(microseconds=Msec)
+    scantime = datetime.datetime(1970, 1, 1) + deltSec + deltMSec
+    return scantime
+
+def get_radar_info(filename):
+    """
+    根据雷达名称找雷达的经纬度信息
+    :param filename:
+    :return:(lat(deg), lon(deg), elev(m), frequency(GHZ))
+    """
+    name = os.path.basename(filename)
+    station_id = [int(name[idx:idx+4]) for idx in range(len(name)-4) if name[idx:idx+4].isdigit()][0]
+    if station_id not in radar_info.index:
+        station_id = 9250 ###找不到站点信息返回南京雷达
+    return radar_info.loc[station_id, "Latitude"], radar_info.loc[station_id, "Longitude"],\
+           radar_info.loc[station_id, "Elevation"], radar_info.loc[station_id, "Frequency"]
+
+def get_radar_type(filename):
+    """
+    根据雷达名称找雷达类型
+    :param filename:
+    :return:
+    """
+    name = os.path.basename(filename)
+    station_id = [int(name[idx:idx + 4]) for idx in range(len(name) - 4) if name[idx:idx + 4].isdigit()][0]
+    if station_id not in radar_info.index:
+        return None
+    Datatype = radar_info.loc[station_id, "Datatype"]
+    if Datatype in ["SA", "SB", "CB", "SC", "CD"]:
+        return "SAB"
+    elif Datatype in ["CC", "CCJ"]:
+        return "CC"
+    else:
+        return None
+
+def radar_format(filename):
+    if hasattr(filename, 'read'):
+        return filename
+    fh = _prepare_for_read(filename)
+    flag = fh.read(28)
+    size = len(fh.read()) + 28
+    fh.seek(100, 0)
+    sc_flag = fh.read(9)
+    fh.seek(116, 0)
+    cc_flag = fh.read(9)
+    fh.close()
+    if flag[:4] == b'RSTM':
+        return "WSR98D"
+    elif flag[14:16] == b'\x01\x00':
+        return "SAB"
+    elif (size-1024)%3000 == 0 and cc_flag == b"CINRAD/CC":
+        return "CC"
+    elif (size-1024)%4000 == 0 and (sc_flag == b"CINRAD/SC" or sc_flag == b"CINRAD/CD"):
+        return "SC"
+    else:
+        return get_radar_type(filename)
+

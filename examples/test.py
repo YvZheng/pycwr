e43e6eb0fe7386f357dd5c709d6ca785ad88c10a
zy <zhengyunuist@gmail.com>
2019-09-24 10:30:48 +0800

    test libs

--- examples/test.py
+++ examples/test.py
@@ -1,7 +1,12 @@
 import sys
-sys.path.append("../")
-from libs.io.auto_io import radar_io
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "NuistRadar"))
+from NuistRadar.io.auto_io import radar_io
 
-radar_obj = radar_io(r"E:\RadarBaseData\StandardFormat\厦门\Z9592.20160728.111443.AR2.bz2")
+file_cc = r"E:\RadarBaseData\CINRAD-CC\2016070818.00V"
+file_sc = r"E:\RadarBaseData\CINRAD_SC_old\Z_RADR_I_Z9280_20180209132700_O_DOR_SC_CAP.bin"
+file_98d = r"E:\RadarBaseData\郑玉\利奇马\LQM_Z9513_NT\Z_RADR_I_Z9513_20190811234540_O_DOR_SAD_CAP_FMT.bin.bz2"
+
+radar_obj = radar_io(file_98d)
 NRadar = radar_obj.ToNuistRadar()
 PyartRadar = radar_obj.ToPyartRadar()

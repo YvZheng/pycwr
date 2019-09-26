da5686879c4aaadaa7922635b303660dc05a35d0
zy <zhengyunuist@gmail.com>
2019-09-26 20:39:54 +0800

    rewrite configure files

--- NuistRadar/cfgs.py
+++
@@ -1,5 +0,0 @@
-import os
-
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-Radar_info_Path = os.path.join(ROOT_DIR, "data", "radar_info.json")
-CN_shp_path = os.path.join(ROOT_DIR, "data", "CHN_pronvices.shp")
--- NuistRadar/configure/location_config.py
+++ NuistRadar/configure/location_config.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
+import cartopy.io.shapereader as shpreader
 import pandas as pd
-from ..cfgs import Radar_info_Path
+import os
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+Radar_info_Path = os.path.join(ROOT_DIR, "data", "radar_info.json")
+CN_shp_path = os.path.join(ROOT_DIR, "data", "CHN_pronvices.shp")
 
 radar_info = pd.read_json(Radar_info_Path)
+CN_shp_info = shpreader.Reader(CN_shp_path)

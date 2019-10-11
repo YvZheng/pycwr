95d4052f74df160762401fb5742fc9f386824d46
zy <zhengyunuist@gmail.com>
2019-10-11 13:41:47 +0800

    add setup.py by bugsuse

---
+++ NuistRadar/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+added by bugsuse(https://github.com/bugsuse)
+"""
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration('NuistRadar', parent_package, top_path)
+    config.add_subpackage('configure')
+    config.add_subpackage('io')     # io first to detect if RSL is missing.
+    config.add_subpackage('core')
+    config.add_subpackage('draw')
+    config.add_subpackage('interp')
+    config.add_data_dir('data')
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())

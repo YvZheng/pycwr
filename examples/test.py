29fa91deedd889c8bf875724523846489981ba02
YvZheng <zhengyunuist@gmail.com>
2019-11-25 21:21:51 +0800

    add example test

--- examples/test.py
+++ examples/test.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 from NuistRadar.io.auto_io import radar_io
-from NuistRadar.io.SCFile import SCBaseData
-from NuistRadar.draw.VerticalSectionPlot import VerticalSection
-import numpy as np
+from NuistRadar.draw.SingleRadarPlotMap import RadarGraphMap
+from NuistRadar.draw.SingleRadarPlot import RadarGraph
+from NuistRadar.qc.attenuation import correct_attenuation_HB, correct_attenuation
 import matplotlib.pyplot as plt
 
-#file = r"E:\RadarBaseData\CINRAD-SA\温州\2015080816.59A"
-file = r"C:\Users\zy\Desktop\Z_RADR_I_Z9831_20190427200200_O_DOR_SC_CAP.bin"
+file = r"E:\RadarBaseData\CINRAD-SA\温州\2015080816.59A"
+#file = r"E:\RadarBaseData\CINRAD-CC\2016070817.03V"
 #radar = SCBaseData(file)
 Radar = radar_io(file)
 NRadar = Radar.ToNuistRadar()
 PyartRadar = Radar.ToPyartRadar()
+
+dbz = NRadar.fields[0]["dBZ"]
+y = correct_attenuation_HB(dbz)
+graph = RadarGraph(NRadar)
+z = correct_attenuation(dbz, "X")
+#graph.plot(0, "dBZ", continuously=False)
+graph.simple_plot_ppi(dbz.range, dbz.azimuth, dbz.elevation, dbz, cmap="CN_ref")
+plt.show()

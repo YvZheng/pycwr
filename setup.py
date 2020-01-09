6af8589f706be2046b2d6be7e50c5f3930d90901
YvZheng <zhengyunuist@gmail.com>
2020-01-09 21:45:47 +0800

    fix pyart radar obj bugs in v0.2.14

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ CLASSIFIERS = [
     'Operating System :: Microsoft :: Windows']
 setup(
     name=DISTNAME,
-    version="0.2.13",
+    version="0.2.14",
     author=AUTHOR,
     license=LICENSE,
     author_email=AUTHOR_EMAIL,
